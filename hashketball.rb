2a2db6aa8e9dd8617b7adb8608f0a95a454da205
Luis Garcia <l.garcia24@hotmail.com>
2020-07-24 18:21:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
        :team_name => "Brooklyn Nets",
        :colors => ["Black", "White"],
        :players => [
-         {
+        {
          :player_name => "Alan Anderson",
          :number => 0,
          :shoe => 16,
@@ -15,8 +15,8 @@ def game_hash
          :blocks => 1,
          :slam_dunks => 1
         },
-         {
-          :player_name => "Reggie Evans",
+        {
+         :player_name => "Reggie Evans",
          :number => 30,
          :shoe => 14,
          :points => 12,
@@ -24,9 +24,10 @@ def game_hash
          :assists => 12, 
          :steals => 12,
          :blocks => 12,
-         :slam_dunks => 7},
-         
-          {:player_name => "Brook Lopez",
+         :slam_dunks => 7
+        },
+        {
+         :player_name => "Brook Lopez",
          :number => 11,
          :shoe => 17,
          :points => 17,
@@ -34,9 +35,10 @@ def game_hash
          :assists => 10, 
          :steals => 3,
          :blocks => 1,
-         :slam_dunks => 15},
-         
-         {:player_name => "Mason Plumlee",
+         :slam_dunks => 15
+        },
+        {
+         :player_name => "Mason Plumlee",
          :number => 1,
          :shoe => 19,
          :points => 26,
@@ -44,9 +46,10 @@ def game_hash
          :assists => 6, 
          :steals => 3,
          :blocks => 8,
-         :slam_dunks => 5},
-         
-         {:player_name => "Jason Terry",
+         :slam_dunks => 5
+        },
+        {
+         :player_name => "Jason Terry",
          :number => 31,
          :shoe => 15,
          :points => 19,
